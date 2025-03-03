9d24ab8e86ad2c82384eb295a9220a34ded3ff97
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-03-04 00:31:06 +0700

    fix: setup header and navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import Image from "next/image"
+import AuthModal from "./AuthModal"
+
+const Navbar = () => {
+    return (
+        <div className="flex py-5 items-center justify-between">
+            <Link href="/" className="flex items-center gap-2">
+                <Image src="/logo.png" alt="Logo" width={40} height={40} />
+                <h4 className="text-3xl font-semibold">Ca<span className="text-blue-500">lendary</span></h4>
+            </Link>
+
+            <AuthModal />
+        </div>
+    )
+}
+
+export default Navbar
