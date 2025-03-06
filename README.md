93e15f88019603cc652fe2fe67006b16beabebff
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-03-06 17:41:08 +0700

    fix: add uploadthing for image uploading

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-
 - Zod 
 - Conform
 - Nylas
+- Uploadthing
 ## Features
 
 - [x] Authentication
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    domains: ["utfs.io", "lh3.googleusercontent.com", "avatars.githubusercontent.com"],
     remotePatterns: [
       {
         protocol: "https",
@@ -13,6 +14,12 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
         port: "",
         pathname: "/**",
       },
