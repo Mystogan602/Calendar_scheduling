0b34e42fe78d03c7d1bb12bf06ef2dbd1550320b
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-03-11 17:21:29 +0700

    feat: calendar grid ui

---
+++ src/app/components/bookingForm/CalendarGrid.tsx
@@ -0,0 +1,63 @@
+import { useCalendarGrid, useLocale, DateValue } from "react-aria";
+import {
+  DateDuration,
+  endOfMonth,
+  getWeeksInMonth,
+} from "@internationalized/date";
+import { CalendarState } from "@react-stately/calendar";
+import CalendarCell from "./CalenderCell";
+
+export default function CalendarGrid({
+  state,
+  offset = {},
+  isDateUnavailable,
+}: {
+  state: CalendarState;
+  offset?: DateDuration;
+  isDateUnavailable?: (date: DateValue) => boolean;
+}) {
+  const startDate = state.visibleRange.start.add(offset);
+  const endDate = endOfMonth(startDate);
+  let { locale } = useLocale();
+  const weeksInMonth = getWeeksInMonth(startDate, locale);
+  let { gridProps, headerProps, weekDays } = useCalendarGrid(
+    {
+      startDate,
+      endDate,
+      weekdayStyle: "short",
+    },
+    state
+  );
+  return (
+    <table {...gridProps} cellPadding={0} className="flex-1">
+      <thead {...headerProps} className="text-sm font-medium">
+        <tr>
+          {weekDays.map((day, index) => (
+            <th key={index}>{day}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {[...new Array(weeksInMonth).keys()].map((weekIndex) => (
+          <tr key={weekIndex}>
+            {state
+              .getDatesInWeek(weekIndex, startDate)
+              .map((date, i) =>
+                date ? (
+                  <CalendarCell
+                    key={i}
+                    state={state}
+                    date={date}
+                    currentMonth={startDate}
+                    isUnavailable={isDateUnavailable?.(date)}
+                  />
+                ) : (
+                  <td key={i} />
+                )
+              )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
